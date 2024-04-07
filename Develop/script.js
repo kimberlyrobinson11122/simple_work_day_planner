9991aeb4a12ce22d7082ac66119f40452c32b16d
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-04-07 16:40:29 -0700

    updated js code to properly store information, updated readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
 
-  // TODO: Add a listener for click events on the save button. This code should use the ID in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? 
-  // How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
-  
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. 
-    
-  //$( "li" ).each(function( index ) {
-//   console.log( index + ": " + $( this ).text() );
-// });
-  //function scope, lexical scope (function(index) - scoped to a function
   $(".time-block").each(function(index) {
     const currentHour = $(this).attr("id").replace("hour-", "");
     if (dayjs().hour() < currentHour) {
@@ -26,39 +11,49 @@ $(function () {
     if (dayjs().hour() === parseInt (currentHour)) {
       $(this).addClass("present");
     }
-  });
+    });
 
   dayjs().hour() // gets current hour
   //console.log(dayjs().hour());
 
+  //save button, event listener
+  $(".saveBtn").click(function() {
+      const task = $(this).siblings("textarea").val()
+      const currentHour = $(this).attr("data-hour");
+      console.log(currentHour);
+
+    //local storage save task
+    localStorage.setItem(`hour-${currentHour}`, task);
+    console.log(`user text at ${currentHour} saved to local storage`);  
+    });
 
 
+  //get items from local storage to textarea val
+  $(`.hourTextArea`).each(function() {
+      const currentTextArea = $(this).find("textarea");
+      const currentHour = $(this).attr("id").replace("hour-", "");
 
-  //$( this ).addClass( "foo" );
-// });
- 
-// // Instead, you should rely on implicit iteration:
-// $( "li" ).addClass( "bar" );
-  //HINTS: How can the id attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+      if (localStorage.getItem(`hour-${currentHour}`)) {
+        currentTextArea.val(localStorage.getItem(`hour-${currentHour}`));
+      }
+      });
 });
 
 // const dayjs = require('dayjs')
 //import dayjs from 'dayjs' // ES 2015
 console.log(dayjs().format());
 
+//changes saved alert
+$(function() {
+  $(".saveBtn").click(function() {
+    alert("Your changes have been saved successfully")
+  });
+});
 
 
 //current date
-var currentDateEl = document.getElementById("currentDay");
-var currentDate = new Date();
+const currentDateEl = document.getElementById("currentDay");
+const currentDate = new Date();
 var options = {
     weekday: 'long',
     year: 'numeric',
@@ -67,3 +62,56 @@ var options = {
     };
 
     currentDateEl.textContent = currentDate.toLocaleDateString('en-US', options);
+
+  //Bad attempts
+  //when saveBtn is clicked for a specific hour (id+textarea)
+  // const textAreas = $('.hourTextArea');
+  // console.log(textAreas);
+  // const saveButtons = document.querySelectorAll('.saveBtn');
+  
+  // //each button needs an event listener
+  // saveButtons.forEach(button => {
+  //   button.addEventListener('click', function() {
+    
+  //   const task = $(this).siblings("textarea").val()
+  //   
+  //   console.log(currentHour);
+
+  //     localStorage.setItem(`hour-${currentHour}`, task);
+  //     console.log(`user text at ${currentHour} saved to local storage`);
+  //   })
+  // })
+
+  // //localStorage.getItems
+  // for(let i=0; i < localStorage.length; i++) {
+  //   console.log(localStorage.getItem(localStorage.key(i)));
+  // }
+
+  //   textAreas.each((index, timeBlockArea) => {
+  //   console.log(timeBlockArea.id);
+   
+  //   const currentTextArea = $(this).siblings("textarea");
+  //   console.log(currentTextArea.val());
+  //   //if timeBlockArea id = localStorage key then pop the text area with localStorage val
+     
+  //     if (localStorage.getItem(timeBlockArea.id)) {
+  //         currentTextArea.val(localStorage.getItem(timeBlockArea.id))
+  //     }
+  // });
+
+  // //class=hourTextArea
+  //button data-hour="XX" (XX is the time/hour)
+  //localStorage.getItem
+
+
+
+// // Instead, you should rely on implicit iteration:
+// $( "li" ).addClass( "bar" );
+  //HINTS: How can the id attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  // id="hour-10"
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
--- README.md
+++ README.md
@@ -1,1 +1,101 @@
-# Work Day Scheduler Starter Code
+# Work Day Planner
+
+#### [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) 
+
+## Screenshot of Initial Build
+
+Screenshot of the first build:
+
+Initial Build for Simple Day Planner for Work ![screen shot of initial build](./Assets/work_day_planner.jpg)
+
+## Table of Contents
+
+PROJECT OVERVIEW
+
+- [Project Title](#work-day-planner)
+
+- [Screenshot](#screenshot-of-initial-build)
+
+- [Description](#description)
+
+- [Installation](#installation)
+
+- [Usage](#usage)
+
+---------------------
+
+DEVELOPMENT ODYSSEY
+
+- [Challenges](#challenges)
+
+- [Test](#credits)
+
+- [Contributions](#contributions)
+
+- [Questions or Issues](#questions-issues)
+
+
+---------------------
+
+ADDITIONAL INFORMATION
+
+- [Credits](#credits)
+
+- [License](#license)
+
+- [Authors and acknowledgment](#authors-and-acknowledgment)
+
+- [Project Status](#project-status)
+
+- [Future Features](#future-features)
+
+
+## Description
+As an employee with a busy schedule, I want to add important events to my daily planner, single use for that day so that I can manage my time effectively. This is a web based tool at this time. 
+
+
+## Installation
+No installtion needed, at this time
+
+
+## Usage
+Once the page is loaded you will just click in the time block you'd like to change, type what you'd like put in there, and click the save button on the right of that specific time block. There will be an alert button that comes up once properly saved. 
+
+
+## Challenges
+
+1. Getting the time blocks to show the approrpriate time
+2. Getting the items stored to local storage
+3. Getting the items to retrieve upon loading the page
+
+## Test
+interface testing as needed
+
+Consistent testing throughout each iteration prior to deployment. 
+
+## Contributions
+Please submit all requests through my GitHub provided later in this readme
+
+This is in a steady state until feature requests are submitted to the above email address and will be considered with owner approval.
+
+## Questions or Issues
+If you have any questions or encounter any issues, please don't hesitate to contact me through either of the following methods:
+
+[GitHub Profile](https://github.com/kimberlyrobinson11122)
+
+Email: me@me.com
+
+## Credits
+The project was developed iteratively with starter code.
+
+## License
+This application/project is covered under the MIT License.
+
+## Authors and acknowledgment
+Appreciation to the incredible Instructor, TAs, Tutors at EdX Berkeley.
+
+## Project Status
+This initial build is complete and was built to project specification.
+
+## Future Features
+Would like to build more functionality to this simple planner, to do list at the bottom, and checkbox of daily routines to run through.
